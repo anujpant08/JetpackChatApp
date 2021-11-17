62f1f84ff13cdcea480d219853e105d07dd53f43
anujpant08 <anujpant2@gmail.com>
2021-11-17 22:19:49 +0530

    Refining login page and sign up page.

--- app/src/main/java/com/minimaldev/android/jetpackchatapp/MainActivity.kt
+++ app/src/main/java/com/minimaldev/android/jetpackchatapp/MainActivity.kt
@@ -1,30 +1,22 @@
 package com.minimaldev.android.jetpackchatapp
 
-import android.graphics.Color
-import android.icu.lang.UCharacter
 import android.os.Bundle
-import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement.Center
-import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.EditCommand
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,36 +26,41 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackChatAppTheme{
+            JetpackChatAppTheme {
                 SignInScreen()
             }
         }
     }
 }
+
 @Composable
 fun SignInScreen() {
-    var username by remember{ mutableStateOf("")}
-    var password by remember{ mutableStateOf("")}
-    Column (
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.purple_200_background)),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-    ){
-        Text(text = "Welcome to JetChat!",
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Welcome to JetChat!",
             color = colorResource(id = R.color.white),
             fontWeight = FontWeight.Bold,
             fontSize = 32.sp,
             textAlign = TextAlign.Center
         )
-        Spacer(modifier = Modifier
-            .fillMaxWidth(1f)
-            .height(16.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(16.dp)
+        )
         OutlinedTextField(
             value = username,
-            onValueChange = {username = it},
-            label = {Text("Username")},
+            onValueChange = { username = it },
+            label = { Text("Username") },
             textStyle = TextStyle(
                 color = colorResource(id = R.color.white),
                 fontSize = 16.sp
@@ -76,13 +73,15 @@ fun SignInScreen() {
                 cursorColor = colorResource(id = R.color.white)
             )
         )
-        Spacer(modifier = Modifier
-            .fillMaxWidth(1f)
-            .height(8.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(8.dp)
+        )
         OutlinedTextField(
             value = password,
-            onValueChange = {password = it},
-            label = {Text("Password")},
+            onValueChange = { password = it },
+            label = { Text("Password") },
             textStyle = TextStyle(
                 color = colorResource(id = R.color.white),
                 fontSize = 16.sp
@@ -96,9 +95,11 @@ fun SignInScreen() {
                 cursorColor = colorResource(id = R.color.white)
             )
         )
-        Spacer(modifier = Modifier
-            .fillMaxWidth(1f)
-            .height(16.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(16.dp)
+        )
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier.padding(12.dp),
@@ -110,8 +111,27 @@ fun SignInScreen() {
         ) {
             Text(
                 text = "LOGIN",
-                fontSize = 16.sp)
+                fontSize = 16.sp
+            )
         }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .height(8.dp)
+        )
+        Text(
+            text = "Not a user? Sign up here.",
+            modifier = Modifier.clickable(
+                onClick = {
+
+
+                }
+            ),
+            color = colorResource(id = R.color.purple_200_dark),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
