a079a9c8fa848aab16e8e28890ca7dbfd346844e
anujpant08 <anujpant2@gmail.com>
2021-11-17 20:21:26 +0530

    Designing login page.

--- app/src/main/java/com/minimaldev/android/jetpackchatapp/ui/theme/Theme.kt
+++ app/src/main/java/com/minimaldev/android/jetpackchatapp/ui/theme/Theme.kt
@@ -5,15 +5,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
+import com.minimaldev.android.jetpackchatapp.R
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
+        primary = Purple200Background,
         primaryVariant = Purple700,
         secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
+        primary = Purple200Background,
         primaryVariant = Purple700,
         secondary = Teal200
 
